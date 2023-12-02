3e82971a6e9190d36919397cd962083174379415
Pacahar <s.kirill29@gmail.com>
2023-12-02 08:10:56 +0300

    1st gen of KR

---
+++ KR/script.js
@@ -0,0 +1,12 @@
+//Слайдер для новостной ленты
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n){
+    showSlides(slideIndex += n)
+}
+
+function showSlides(n){
+    var i;
+    var slides = document.getElementsByClassName("item");
+    
+}
